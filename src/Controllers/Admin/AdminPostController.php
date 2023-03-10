a0157dce7ca6976694a78d28960ffb9796bf6c27
Nonoland <dartoisn@hotmail.fr>
2023-03-10 14:27:06 +0100

    feat(Post): add and edit post

--- src/Controllers/Admin/AdminPostController.php
+++ src/Controllers/Admin/AdminPostController.php
@@ -11,26 +11,71 @@ class AdminPostController extends AdminController
     #[Route('GET', '/admin/posts')]
     public function indexPosts(): void
     {
+        $posts = Post::getAllPosts();
+
         $template = $this->getTwig()->load('admin/posts/posts.twig');
-        echo $template->render();
+        echo $template->render([
+            'posts' => $posts
+        ]);
     }
 
     #[Route(['GET', 'POST'], '/admin/posts/new')]
-    public function postNew()
+    public function postNew(): void
     {
         $request = $this->getRequest();
 
         if ($request->getIsset('post_submit') &&
+            $request->getIsset('post_description') &&
             $request->getIsset('post_title') &&
             $request->getIsset('post_body')) {
+
             $post = new Post();
             $post->setTitle($request->getValuePost('post_title'));
+            $post->setDescription($request->getValuePost('post_description'));
             $post->setBody($request->getValuePost('post_body'));
             $post->setIdUser(1);
             $post->add();
+
+            $this->redirect("/admin/posts");
         }
 
         $template = $this->getTwig()->load('admin/posts/new.twig');
         echo $template->render();
     }
+
+    #[Route(['GET', 'POST'], '/admin/posts/edit/{id_post:int}')]
+    public function postEdit(array $params): void
+    {
+        if (!$post = new Post($params['id_post'])) {
+            return;
+        }
+
+        $request = $this->getRequest();
+
+        if ($request->getIsset('post_submit') &&
+            $request->getIsset('post_description') &&
+            $request->getIsset('post_title') &&
+            $request->getIsset('post_body')) {
+
+            $post->setTitle($request->getValuePost('post_title'));
+            $post->setDescription($request->getValuePost('post_description'));
+            $post->setBody($request->getValuePost('post_body'));
+            $post->update();
+        }
+
+        $template = $this->getTwig()->load('admin/posts/edit.twig');
+        echo $template->render([
+            'post' => $post
+        ]);
+    }
+
+    #[Route(['GET'], '/admin/posts/delete/{id_post:int}')]
+    public function postDelete(array $params): void
+    {
+        $post = new Post((int)$params['id_post']);
+        $post->delete();
+
+        $this->redirect('/admin/posts');
+    }
+
 }
