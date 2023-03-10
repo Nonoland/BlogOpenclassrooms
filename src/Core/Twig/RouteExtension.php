78dd6b49691b5f443d0a4ea8085fcde9acc4c4cb
Nonoland <dartoisn@hotmail.fr>
2023-03-10 14:24:50 +0100

    feat(Twig): add Route function
    feat(Twig): add Route function

---
+++ src/Core/Twig/RouteExtension.php
@@ -0,0 +1,38 @@
+<?php
+namespace Nolandartois\BlogOpenclassrooms\Core\Twig;
+
+use Nolandartois\BlogOpenclassrooms\Core\Routing\Dispatcher;
+use Nolandartois\BlogOpenclassrooms\Core\Routing\Route;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class RouteExtension extends AbstractExtension
+{
+    private Dispatcher $dispatcher;
+
+    public function __construct(Dispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('route', [$this, 'getRoute'])
+        ];
+    }
+
+    public function getRoute($routeName)
+    {
+        foreach ($this->dispatcher->getRoutes() as $controller) {
+            /** @var Route $route */
+            foreach ($controller as $route) {
+                if ($route->getRouteName() == $routeName) {
+                    return $route->getRoute();
+                }
+            }
+        }
+
+        return "/";
+    }
+}
